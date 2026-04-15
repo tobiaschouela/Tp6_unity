e1c4ad6d2f41b9c58c956e0594572275f6be99d4
Tobias Chouela <50159086@est.ort.edu.ar>
2026-04-15 14:28:22 -0300

    Initial commit

---
+++ Assets/script/SaludoUIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaludoUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txt_saludo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
